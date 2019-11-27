57c5f3ac538912c0173525df97a852fa803a5656
Иван <pavlovi.v@ya.ru>
2019-11-27 15:23:07 +0300

    Добавлена задача №8

---
+++ src/Task8.java
@@ -0,0 +1,13 @@
+//Задача №8: Цикличный вывод данных на экран
+//Необходимо с помощью цикла вывести на экран числа от 1 до 50.
+
+import java.io.IOException;
+
+public class Task8 {
+    public static void main (String[] args) throws IOException {
+
+        for (int x = 1; x < 51; x++) {
+            System.out.println(x);
+        }
+    }
+}
